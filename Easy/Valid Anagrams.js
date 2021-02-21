44fb787137118ffc68d3301c8012d72f385ad451
gulevskydev <d.gulevsky@mail.ru>
2021-02-21 11:36:33 +0300

    Easy

---
+++ Easy/Valid Anagrams.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function (s, t) {
+  const chars = {};
+  for (let i = 0; i < s.length; i++) {
+    if (chars[s[i]]) chars[s[i]]++;
+    else chars[s[i]] = 1;
+  }
+
+  for (let i = 0; i < t.length; i++) {
+    if (chars[t[i]]) {
+      chars[t[i]]--;
+    } else {
+      return false;
+    }
+  }
+
+  return true;
+};
