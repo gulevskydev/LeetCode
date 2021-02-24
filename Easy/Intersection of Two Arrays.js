e2d660f822b5a6459eca23dfc8b0fc0fd9cdac88
gulevskydev <d.gulevsky@mail.ru>
2021-02-24 21:21:49 +0300

    Easy

---
+++ Easy/Intersection of Two Arrays.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function (nums1, nums2) {
+  return [...new Set(nums1.filter((e) => nums2.indexOf(e) > -1))];
+};
