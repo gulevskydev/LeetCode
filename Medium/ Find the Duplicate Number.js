55fd7f27417d011820a48822737a7cff6f6380ba
gulevskydev <d.gulevsky@mail.ru>
2021-03-03 21:24:21 +0300

    Medium

---
+++ Medium/ Find the Duplicate Number.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findDuplicate = function (nums) {
+  const obj = {};
+  for (let i = 0; i < nums.length; i++) {
+    obj[nums[i]] = ++obj[nums[i]] || 1;
+  }
+  for (let key in obj) {
+    if (obj[key] > 1) return key;
+  }
+};
