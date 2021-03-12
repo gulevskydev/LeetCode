36c14726d9ce18b8d4412d97862afcb3eec2020d
gulevskydev <d.gulevsky@mail.ru>
2021-03-12 12:16:30 +0300

    Easy

---
+++ Easy/ Last Stone Weight.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number[]} stones
+ * @return {number}
+ */
+var lastStoneWeight = function (stones) {
+  while (stones.length > 1) {
+    stones = stones.sort((a, b) => b - a);
+    stones.splice(0, 2, Math.abs(stones[0] - stones[1]));
+  }
+  return stones;
+};
