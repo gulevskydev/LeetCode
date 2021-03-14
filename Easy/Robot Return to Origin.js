df2f94f688a8bb6f1ea4fa419279c38d736fbf2a
gulevskydev <d.gulevsky@mail.ru>
2021-03-14 16:22:43 +0300

    Easy

---
+++ Easy/Robot Return to Origin.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} moves
+ * @return {boolean}
+ */
+var judgeCircle = function (moves) {
+  let x = 0;
+  let y = 0;
+  for (let i = 0; i < moves.length; i++) {
+    if (moves[i] === 'U') y++;
+    else if (moves[i] === 'D') y--;
+    else if (moves[i] === 'L') x++;
+    else if (moves[i] === 'R') x--;
+  }
+  return y === 0 && x === 0;
+};
