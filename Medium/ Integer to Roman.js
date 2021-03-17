0b2b7c970ae0e850e339b92fbafc59409ad47aec
gulevskydev <d.gulevsky@mail.ru>
2021-03-17 21:28:36 +0300

    Medium

---
+++ Medium/ Integer to Roman.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} num
+ * @return {string}
+ */
+var intToRoman = function (num) {
+  var roman = {
+    M: 1000,
+    CM: 900,
+    D: 500,
+    CD: 400,
+    C: 100,
+    XC: 90,
+    L: 50,
+    XL: 40,
+    X: 10,
+    IX: 9,
+    V: 5,
+    IV: 4,
+    I: 1,
+  };
+  let ans = '';
+  for (let key in roman) {
+    const count = Math.floor(num / roman[key]);
+    num -= roman[key] * count;
+    ans += key.repeat(count);
+  }
+  return ans;
+};
