eb31c282a61fd523a5d0be568d090ef376aea967
gulevskydev <d.gulevsky@mail.ru>
2021-03-20 23:51:19 +0300

    Easy

--- Easy/Thousand Separator.js
+++ Easy/Thousand Separator.js
@@ -2,4 +2,18 @@
  * @param {number} n
  * @return {string}
  */
-var thousandSeparator = function (n) {};
+var thousandSeparator = function (n) {
+  let s = '';
+  let c = 3;
+  n = String(n);
+  for (let i = n.length - 1; i >= 0; i--) {
+    if (c === 0) {
+      s += '.' + n[i];
+      c = 2;
+    } else {
+      c--;
+      s += n[i];
+    }
+  }
+  return s.split('').reverse().join('');
+};
